e86ec714704c78bee991284b4d236a2ebbb43c4d
MMailleur <104862908+MMailleur@users.noreply.github.com>
2023-07-10 15:23:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,4 @@ fruityvice_response = requests.get("https://fruitivice.com/api/fruit/watermelon"
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-streamlit.dataframe(fruityvice_normalized)
+# streamlit.dataframe(fruityvice_normalized)
